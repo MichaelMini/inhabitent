d26a2993a8fb1e0afe8939f0a17af0abf748f786
MichaelMini <mcho911@gmail.com>
2016-05-20 11:51:26 -0700

    initial commit

---
+++ themes/inhabitent/front-page.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<p>
+				fornt-page-php
+			</p>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
