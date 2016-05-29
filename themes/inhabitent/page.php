b05f9897269e532f006a101a7226dfd56a508e7d
MichaelMini <mcho911@gmail.com>
2016-05-29 09:08:17 -0700

    find-us.scss&&page.php && sidebar.scss done.

--- themes/inhabitent/page.php
+++ themes/inhabitent/page.php
@@ -10,14 +10,20 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 <h1>Page.php</h1>
-			<?php while ( have_posts() ) : the_post(); ?>
+			<section class="container page-find-us post-page">
+				<div class="post-container">
+					<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+						<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
-			<?php endwhile; // End of the loop. ?>
+					<?php endwhile; // End of the loop. ?>
 
+				</div>
+				<div class="sidebar">
+					<?php get_sidebar(); ?>
+				</div>
+			</section>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
