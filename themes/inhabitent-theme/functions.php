c630b3e50f361dd8f8fc06bddc43fc89a70bc62d
MichaelMini <mcho911@gmail.com>
2016-06-06 16:00:35 -0700

    toggle search button fixed.

--- themes/inhabitent-theme/functions.php
+++ themes/inhabitent-theme/functions.php
@@ -89,6 +89,7 @@ function inhabitent_scripts() {
 	$script_url = get_template_directory_uri() . '/js/scripts.js';
 
 	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'inhabitent_search', $script_url, array( 'jquery' ), false, true );
 
 	wp_enqueue_script( 'inhabitent-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20130115', true );
 
--- themes/inhabitent-theme/searchform.php
+++ themes/inhabitent-theme/searchform.php
@@ -4,10 +4,10 @@
 		<span class="icon-search" aria-hidden="true">
 			<a href="" class="search-btn">
 				<i class="fa fa-search"></i>
+			</a>
 		<label>
 			<input type="search" class="search-field" placeholder="SEARCH AND HIT ENTER ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
-			</a>
 		<span class="screen-reader-text"><?php echo esc_html( 'Search' ); ?></span>
 		</span>
 	</fieldset>
